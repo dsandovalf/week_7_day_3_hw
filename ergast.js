328cd304d5288d2ea4f24917498481790a80d234
dsandovalf <dasanflo@gmail.com>
2021-11-15 16:31:58 -0600

    first commit

---
+++ ergast.js
@@ -0,0 +1,57 @@
+async function doAPICall(){
+    let year = document.querySelector(".year").value
+    let round = document.querySelector(".round").value
+
+
+    result = await axios.get(`https://ergast.com/api/f1/${year}/${round}/driverStandings.json`)
+        .catch((e)=>{console.error(e)
+            alert("Bad year or round")})
+                .finally(console.log("API request is Over"));
+    console.log(result);
+    console.log(result.data);
+    
+    result = result.data;
+
+    ds = result.MRData.StandingsTable.StandingsLists[0].DriverStandings
+    console.log(ds)
+
+
+    for (let d of ds){
+        console.log(d)
+
+        tbody = document.getElementsByTagName('tbody')[0];
+
+        tr = document.createElement('tr');
+        tbody.appendChild(tr);
+
+        td = document.createElement('td');
+        td.innerText = d.Driver.givenName;
+        tr.appendChild(td)
+
+        td = document.createElement('td');
+        td.innerText = d.Driver.familyName;
+        tr.appendChild(td)
+
+        td = document.createElement('td');
+        td.innerText = d.Driver.dateOfBirth;
+        tr.appendChild(td)
+
+        td = document.createElement('td');
+        td.innerText = d.position;
+        tr.appendChild(td)
+
+        td = document.createElement('td');
+        td.innerText = d.wins;
+        tr.appendChild(td)
+
+        td = document.createElement('td');
+        td.innerText = d.Driver.nationality;
+        tr.appendChild(td)
+
+        td = document.createElement('td');
+        td.innerText = d.Constructors[0].name;
+        tr.appendChild(td)
+
+        
+    };
+};
